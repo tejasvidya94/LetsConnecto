227da8a02393d7895d49f8b4ac45e659f403d4cc
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-03-31 11:18:35 +0530

    frontend folder reinitialized.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,10 @@
-import Navbar from "./components/Navbar"
-import { Routes, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import SignUpPage from "./pages/SignUpPage"
-import LogInPage from "./pages/LogInPage"
-import ProfilePage from "./pages/ProfilePage"
-
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LogInPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
+
     </>
   )
 }
--- frontend/src/lib/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const axiosInstance = axios.create({
-    baseURL: "localhost:5001/api",
-    withCredentials: true,
-})
