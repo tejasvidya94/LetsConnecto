60d76730aa3757c0af7c7a3d7e9466fd49af0fb4
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-03-31 10:53:00 +0530

    just semi column added.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { login, logout, signup, updateProfile, checkAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/signup", signup);
 
