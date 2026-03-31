95276789aec618ef9564a666288af4f6174cbc01
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-03-31 12:59:38 +0530

    installed and initialized react-router-dom and created files for signup, login, forgotpass and homepage and all other.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import Navbar from './components/Navbar.jsx'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import ForgotPassPage from './pages/ForgotPassPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+
 function App() {
 
   return (
     <>
-      Hello
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/forgot-pass' element={<ForgotPassPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+        </Routes>
+      </div>
     </>
   )
 }
---
+++ frontend/src/pages/ForgotPassPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ForgotPassPage = () => {
+    return (
+        <div>
+            forgot password page
+        </div>
+    )
+}
+
+export default ForgotPassPage
