8e0c00db0dd28a1ebdcceab00351f83837b2dd1e
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-06-01 21:52:40 +0530

    app is prepared for deployment here actually

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,12 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
+
+const BASE_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:3000"
+        : "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
