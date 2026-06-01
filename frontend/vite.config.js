319b9ee96ba066556f8bf794ef30d9c8e5bad545
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-06-01 23:52:37 +0530

    I hope it will solve problem as i have just removed path imported into vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "node:path";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
