3c724903ef6a0960f1cce972e07dd3420d62d915
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-06-01 21:51:43 +0530

    App virsion prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,42 @@ import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
 import app from './src/index.js';
 import { server } from './src/lib/socket.io.js';
+import path from 'path';
+import express from 'express';
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// add react build here to connect frontend to backend.
+const __dirname = path.resolve();
+
+app.use(
+    express.static(
+        path.join(
+            __dirname,
+            "../frontend/dist"
+        )
+    )
+);
+
+app.use((req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "../frontend/dist/index.html"
+        )
+    );
+});
+
+// // production apps do it.
+// app.get(/^(?!\/api).*/, (req, res) => {
+//     res.sendFile(
+//         path.join(
+//             __dirname,
+//             "../frontend/dist/index.html"
+//         )
+//     );
+// });
 
 const PORT = process.env.PORT;
 
